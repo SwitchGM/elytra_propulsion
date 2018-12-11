d5e843fcb2c46beb3639af4be9b06c62cc6c217b
Joseph Amphlett <joe@amphlett.net>
2018-12-11 21:36:07 +0000

    first working verison

--- src/com/Switch/elytra_propulsion/Main.java
+++ src/com/Switch/elytra_propulsion/Main.java
@@ -1,5 +1,6 @@
 package com.Switch.elytra_propulsion;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
@@ -8,6 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 import java.util.HashMap;
 
@@ -19,24 +21,47 @@ import java.util.HashMap;
  */
 public class Main extends JavaPlugin implements Listener {
 
-    HashMap<Player, Long> cooldownMap = new
+    private HashMap<Player, Long> cooldownMap = new HashMap<>();
 
     @Override
     public void onEnable() {
         setupConfigFile();
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
     public void onClick(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        FileConfiguration config = this.getConfig();
 
-        Material material = Material.getMaterial(config.getString("material"));
+        Player player = event.getPlayer();
 
         if (player.isGliding()) {
+
+            FileConfiguration config = this.getConfig();
+            Material material = Material.getMaterial(config.getString("material"));
+
             if (player.getInventory().getItemInMainHand().getType() == material) {
                 if (event.getAction().equals(Action.LEFT_CLICK_AIR)) {
+                    if (cooldownMap.containsKey(player)) {
 
+                        int cooldown = config.getInt("cooldown");
+
+                        if (cooldownMap.get(player) + (cooldown * 1000) <= System.currentTimeMillis()) {
+                            cooldownMap.remove(player);
+
+                        } else {
+                            String cooldownMessage = config.getString("cooldown_message");
+                            player.sendMessage(ChatColor.translateAlternateColorCodes('&', cooldownMessage));
+                            return;
+
+                        }
+                    }
+
+                    int multiplier = config.getInt("multiplier");
+                    Vector vector = player.getLocation().getDirection();
+                    Vector newVector = new Vector(vector.getX() * multiplier, vector.getY() * multiplier, vector.getZ() * multiplier);
+
+                    player.setVelocity(newVector);
+                    cooldownMap.put(player, System.currentTimeMillis());
                 }
             }
         }
